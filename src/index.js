7aec71dcabf46c3071a1f669ef442fd80a423f4a
Axyl07 <aryanchauhan000777@gmail.com>
2025-03-13 13:57:55 +0530

    Fix jest by importing expect

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import test from "./test";
-test();
+import { testingFn } from "./script";
+console.log(testingFn());
---
+++ src/script.js
@@ -0,0 +1,3 @@
+export function testingFn() {
+  return "is this battleship working>/?";
+}
