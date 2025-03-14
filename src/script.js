81c8ea253d5fc5b08f7bf14adb482bf90a03b592
Axyl07 <aryanchauhan000777@gmail.com>
2025-03-14 10:30:25 +0530

    Gameboard placing and attacking done

--- src/script.js
+++ src/script.js
@@ -3,10 +3,12 @@
 // }
 
 export class Ship{
-  constructor(length=0,hitCount=0,sunkStatus=false) {
+  constructor(length=0,hitCount=0,sunkStatus=false,xCoordinate,yCoordinate) {
     this.length = length;
     this.hitCount = hitCount;
     this.sunkStatus = sunkStatus;
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
   }
   hit() {
     this.hitCount++;
@@ -17,5 +19,25 @@ export class Ship{
     } else this.sunkStatus = true;
     return this.sunkStatus;
   }
+}
 
+export class Gameboard{
+  missedShotsArray = [];
+  placedShips = [];
+  place(x, y, shipName) {
+    shipName.xCoordinate = x;
+    shipName.yCoordinate = y;
+    this.placedShips.push(shipName);
+  }
+  receiveAttack(x, y) {
+    this.placedShips.forEach(ship => {
+      if (x==ship.xCoordinate&&y==ship.yCoordinate) {
+        //hit
+        ship.hit()
+      } else {
+        //missedShot
+        this.missedShotsArray.push([x, y]);
+      }
+    });
+ }
 }
--- src/script.test.js
+++ src/script.test.js
@@ -1,10 +1,10 @@
 // import { testingFn } from "./script.js";
 import { describe, expect, test } from "@jest/globals";
-import { Ship } from "./script";
+import { Ship,Gameboard } from "./script";
 // test("testing", () => {
 //   expect(testingFn()).toBe("is this battleship working>/?");
 // });
-
+//shipTests:
 test("Testing Ship for sink", () => {
     const thesus = new Ship(3,0,false);
     thesus.hit();
@@ -15,6 +15,18 @@ test("Testing Ship for sink", () => {
 test("Testing Ship for not sink", () => {
     const thesus2 = new Ship(2,0,false);
     thesus2.hit();
-    console.log(thesus2)
+    // console.log(thesus2)
     expect(thesus2.isSunk()).toBe(false);
+})
+//GameBoard Tests:
+test('Place ship test', () => {
+    const ship1 = new Ship(1, 0, false);
+    const gameboard1 = new Gameboard();
+    gameboard1.place(0, 0, ship1);
+    expect(gameboard1.missedShotsArray).toEqual([]);
+    gameboard1.receiveAttack(0, 1);
+    expect(gameboard1.missedShotsArray).toEqual([[0,1]]);
+    gameboard1.receiveAttack(0, 0);
+    console.log(ship1);
+    expect(ship1.isSunk()).toBe(true);
 })
