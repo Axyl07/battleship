107e894b77d852717d8051b52a3b5daa7ba1a19c
Axyl07 <aryanchauhan000777@gmail.com>
2025-03-30 11:50:06 +0530

    Rearranged render logic to show specific cells hitting ship and different style when ship is sunk and disable cells after one click

--- src/domHit.js
+++ src/domHit.js
@@ -1,14 +1,19 @@
 import renderGameboard from "./renderGameboard";
 
 export default function play(human, computer) {
-    console.log(human);
-    console.log(computer);
+  console.log(human);
+  console.log(computer);
   const humanGrid = document.querySelector(".humanGrid");
   const computerGrid = document.querySelector(".computerGrid");
+  const humanGameboard = human.gameboard;
+  const computerGameboard = computer.gameboard;
+  const humanPositions = humanGameboard.positions;
+  const computerPositions = computerGameboard.positions;
+
   const cells = document.querySelectorAll(".col");
   //   console.log(cells);
   cells.forEach((cell) => {
-      cell.addEventListener("click", () => {
+    cell.addEventListener("click", () => {
       const idString = cell.id;
       console.log(idString);
       const idStringLength = idString.length;
@@ -20,13 +25,54 @@ export default function play(human, computer) {
         playerName = idString.substring(0, 5);
         x = idString.substring(10, 11);
         y = idString.substring(12, 13);
-        human.gameboard.receiveAttack(x, y);
+        humanGameboard.receiveAttack(x, y);
+        //render hit shots
+        const humanShip = humanPositions[x][y];
+        if (typeof humanShip === "object") {
+          if (humanShip.hitCount > 0) {
+            const gridCell = document.getElementById(
+              `${humanGrid.className}-${x},${y}`,
+            );
+            gridCell.style.backgroundColor = "yellow";
+            cell.disabled = "true";
+          }
+          if (humanShip.isSunk()) {
+            renderGameboard(humanGameboard, humanGrid);
+            cell.disabled = true;
+          }
+        } else {
+          const gridCell = document.getElementById(
+            `${humanGrid.className}-${x},${y}`,
+          );
+          gridCell.textContent = "•";
+        }
+
       } else {
         //computerCellisClicked
         playerName = idString.substring(0, 8);
         x = idString.substring(13, 14);
         y = idString.substring(15, 16);
-        computer.gameboard.receiveAttack(x, y);
+        computerGameboard.receiveAttack(x, y);
+        //render hit shots
+        const computerShip = computerPositions[x][y];
+        if (typeof computerShip === "object") {
+          if (computerShip.hitCount > 0) {
+            const gridCell = document.getElementById(
+              `${computerGrid.className}-${x},${y}`,
+            );
+            gridCell.style.backgroundColor = "yellow";
+            cell.disabled = "true";
+          }
+          if (computerShip.isSunk()) {
+            renderGameboard(computerGameboard, computerGrid);
+            cell.disabled = true;
+          }
+        } else {
+          const gridCell = document.getElementById(
+            `${computerGrid.className}-${x},${y}`,
+          );
+          gridCell.textContent = "•";
+        }
       }
       console.log(playerName);
       console.log(x, y);
--- src/renderGameboard.js
+++ src/renderGameboard.js
@@ -1,14 +1,16 @@
 export default function renderGameboard(GameBoard, Grid) {
-  const missedShots = GameBoard.missedShots;
+  // const missedShots = GameBoard.missedShots;
   const positions = GameBoard.positions;
   console.log(positions);
-  missedShots.forEach((array) => {
-    const x = array[0];
-    const y = array[1];
-    console.log(`${Grid.className}-${x},${y}`);
-    const cell = document.getElementById(`${Grid.className}-${x},${y}`);
-    cell.textContent = "•";
-  });
+  //render missed shots
+  // missedShots.forEach((array) => {
+  //   const x = array[0];
+  //   const y = array[1];
+  //   console.log(`${Grid.className}-${x},${y}`);
+  //   const cell = document.getElementById(`${Grid.className}-${x},${y}`);
+  //   cell.textContent = "•";
+  // });
+  //render hit shots
   for (let i = 0; i < positions.length; i++) {
     const row = positions[i];
     for (let j = 0; j < row.length; j++) {
@@ -19,16 +21,17 @@ export default function renderGameboard(GameBoard, Grid) {
             `${Grid.className}-${i},${j}`,
           );
             gridCell.textContent = 'X'
-          gridCell.style.backgroundColor = "white";
+          // gridCell.style.backgroundColor = "white";
           gridCell.style.color = "red";
           gridCell.style.border = "1px solid red";
-        } else {
-          const gridCell = document.getElementById(
-            `${Grid.className}-${i},${j}`,
-          );
-          gridCell.style.backgroundColor = "aquamarine";
-          gridCell.style.border = "1px solid yellow";
         }
+        // else {
+        //   const gridCell = document.getElementById(
+        //     `${Grid.className}-${i},${j}`,
+        //   );
+        //   gridCell.style.backgroundColor = "aquamarine";
+        //   gridCell.style.border = "1px solid yellow";
+        // }
       }
     }
   }
